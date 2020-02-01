d59810f87e82bd3e38fb0a7c43086f3ba2a75c73
dschawel <david.schawel@gmail.com>
2020-01-31 18:19:37 -0800

    Got some info to display, working on the others

--- src/content/pages/Vets.js
+++ src/content/pages/Vets.js
@@ -10,25 +10,50 @@ const config = {
         location: 'seattle'
     }
 }
-
 class Vets extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            businesses: []
+        }
+    }
     componentDidMount() {
-        console.log('cdm', config, process.env.REACT_APP_YELP_API_KEY)
+        console.log(`${process.env.REACT_APP_CORS_ANYWHERE}https://api.yelp.com/v3/businesses/search`)
         axios.get(`${process.env.REACT_APP_CORS_ANYWHERE}https://api.yelp.com/v3/businesses/search`, config)
-        .then(response => console.log('success', response))
+        .then((response) => {
+            console.log('success', response)
+            let data = response.data.businesses
+            this.setState({businesses: data})
+            // this.setState({name: data.name})
+            // this.setState({address: data.address1})
+            // this.setState({city: data.city})
+            // this.setState({state: data.state})
+            // this.setState({zip_code: data.zip_code})
+            // this.setState({display_phone: data.display_phone})
+        })
         .catch(err => {
             console.log(err)
         })
     }
+
     
     render(){
+        let businesses = this.state.businesses.map((b, i) => {
+            return (
+            <div key={i}>
+                {b.name}
+            </div>
+            )
+        })
+
         return (
         <div className='results'>
             <h1>Here are some vets!</h1>
-            
+            {businesses}
         </div>
         )
     }
 }
+
 
 export default Vets
